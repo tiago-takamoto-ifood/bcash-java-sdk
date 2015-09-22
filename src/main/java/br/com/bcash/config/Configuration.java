3fb5ccc564139f53553d03776e477f6043668ee3
Raphael Negrisoli Batista <raphael.batista@bcash.com.br>
2015-09-22 18:28:42 -0300

    Add a create transaction request.

---
+++ src/main/java/br/com/bcash/config/Configuration.java
@@ -0,0 +1,12 @@
+package br.com.bcash.config;
+
+public class Configuration {
+
+	public static final String apiHost = "https://api.bcash.com.br/service/";
+
+	public static final String sandboxApiHost = "https://sandbox-api.bcash.com.br/service/";
+
+	public static final String host = "https://www.bcash.com.br/";
+
+	public static final String sandboxHost = "https://sandbox.bcash.com.br/";
+}
--- src/main/java/br/com/bcash/service/TransactionService.java
+++ src/main/java/br/com/bcash/service/TransactionService.java
@@ -1,8 +1,25 @@
 package br.com.bcash.service;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.http.HttpStatus;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.entity.ContentType;
+import org.apache.http.message.BasicNameValuePair;
+
+import br.com.bcash.config.Configuration;
 import br.com.bcash.domain.transaction.TransactionRequest;
 import br.com.bcash.domain.transaction.TransactionResponse;
+import br.com.bcash.http.HttpConnection;
+import br.com.bcash.http.HttpRequest;
+import br.com.bcash.http.HttpResponse;
+import br.com.bcash.http.authentication.OAuth;
 import br.com.bcash.http.authentication.OAuthCredentials;
+import br.com.bcash.util.JsonUtil;
 
 public class TransactionService {
 
@@ -27,8 +44,29 @@ public class TransactionService {
 		this.oAuthCredentials = oAuthCredentials;
 	}
 
-	public TransactionResponse create(TransactionRequest request) {
-		return null;
+	public TransactionResponse create(TransactionRequest request) throws IOException {
+		Map<String, String> authentication = OAuth.generateHeader(oAuthCredentials);
+		HttpRequest httpRequest = new HttpRequest();
+		httpRequest.setUrl(Configuration.sandboxApiHost + SERVICE_URL);
+		httpRequest.setHeaders(authentication);
+
+		List<NameValuePair> nvps = new ArrayList<NameValuePair>();
+		nvps.add(new BasicNameValuePair("encode", "UTF_8"));
+		nvps.add(new BasicNameValuePair("version", "1.0"));
+		nvps.add(new BasicNameValuePair("data", JsonUtil.toJson(request)));
+
+		UrlEncodedFormEntity body = new UrlEncodedFormEntity(nvps);
+		body.setContentType(ContentType.APPLICATION_FORM_URLENCODED.getMimeType());
+		httpRequest.setBody(body);
+
+		HttpConnection httpConnection = new HttpConnection();
+		HttpResponse httpResponse = httpConnection.post(httpRequest);
+
+		if (httpResponse.getStatusCode() != HttpStatus.SC_OK) {
+			throw new IllegalArgumentException(httpResponse.getBody());
+		}
+
+		return JsonUtil.fromJson(httpResponse.getBody(), TransactionResponse.class);
 	}
 
 }
